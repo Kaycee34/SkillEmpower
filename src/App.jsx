eb87824591f2cb1ab1c931dd58ddfaa583610a0d
Kaycee34 <udechukwudestinykelechi945@gmail.com>
2024-12-01 15:14:35 +0100

    fixed vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Pages/Home';
 import Courses from './pages/Courses';
 import Services from './pages/Services';
 import Programs from './pages/Programs';
 import Register from './pages/Register';
-//about/contact
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -18,7 +17,7 @@ const App = () => {
         <Route path="/programs" element={<Programs />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import App from './App';
 
@@ -7,7 +8,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
