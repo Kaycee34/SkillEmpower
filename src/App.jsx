401a2124f4a1a3a0994e391668d9b50005454e25
Kaycee34 <udechukwudestinykelechi945@gmail.com>
2024-12-04 14:58:50 +0100

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from './Pages/Services';
 import Programs from './Pages/Programs';
 import Register from './Pages/Register';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 const App = () => {
